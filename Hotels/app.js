1c4f400f3b9f78d435d04193ef7c59a81ffd51cf
LeontevaMarina <77490776+LeontevaMarina@users.noreply.github.com>
2021-01-18 23:21:21 +0300

    First Commit

---
+++ Hotels/app.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+
+    let elementId = $(this).data('scroll');
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate({
+        scrollTop: elementOffset -70
+    }, 700);
+
+    });
+
+    /* Nav Toggle*/
+
+    let nav = $("#nav");
+
+    $("#navToggle").on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+});
